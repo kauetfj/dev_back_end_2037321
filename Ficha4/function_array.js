ebe875c3701bf7832fd6e274a89efe5b4b35bdf4
Kauet <felix.kauet@gmail.com>
2022-04-06 18:48:01 +0100

    Aula 06/04/2022

---
+++ Ficha4/function_array.js
@@ -0,0 +1,24 @@
+// linha 4
+var array = [];
+
+array.push(
+function(){
+    console.log("Hello World 1, ");
+},
+
+function(){
+    console.log("2, ");
+},
+
+function(){
+    console.log("3");
+})
+
+// for (let i = 0; i < array.length; i++) {
+//     array[i]();
+// }
+
+
+
+
+
